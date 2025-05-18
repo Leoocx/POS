774b0a94982c58280fca665650910835b91d5b43
Leo <161379464+Leoocx@users.noreply.github.com>
2025-05-17 21:47:08 -0300

    Delete POS/src/main/java/com/system/pos/pos/report/RelatorioVendas.java

--- POS/src/main/java/com/system/pos/pos/report/RelatorioVendas.java
+++
@@ -1,8 +0,0 @@
-package com.system.pos.pos.report;
-
-import java.time.LocalDate;
-
-public class RelatorioVendas {
-    public void gerarRelatorioPeriodo(LocalDate inicio, LocalDate fim){
-    }
-}
